a613ae2934458a0dae146526f89b3b1f43ff500f
DESKTOP-28F38VP\user <nkc.syougo.sawada@cmail.com>
2019-07-03 13:36:15 +0900

    車の動き完成

--- Car Game/Assets/3.SAWADA/Car.cs
+++ Car Game/Assets/3.SAWADA/Car.cs
@@ -9,11 +9,11 @@ public class Car : MonoBehaviour
     public float maxSteeringAngle=30;//適用可能な最大ハンドル角度
     public void ApplyLocalPosiitonToVisuals(WheelCollider collider)
     {
-        //Wheelコライダーの子要素がない場合は処理終了
+        // Wheelコライダーの子要素がない場合は処理終了。
         if (collider.transform.childCount == 0)
-         return;
-            //タイヤビジュアルを取得
-            Transform visual = collider.transform.GetChild(0);
+            return;
+        //タイヤビジュアルを取得
+        Transform visual = collider.transform.GetChild(0);
         //コライダーの位置と回転を取得
         Vector3 pos;
         Quaternion q;
@@ -21,7 +21,7 @@ public class Car : MonoBehaviour
         //タイヤビジュアルに、コライダーの値を設定。
         //単純な車の場合、Z軸を90f回転させる必要がある
         visual.transform.position = pos;
-        visual.transform.rotation = q * Quaternion.Euler(0f, 0f,0f); ;
+        visual.transform.rotation = q  ;
         
     }
     // Start is called before the first frame update
