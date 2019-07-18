16cc90c2ec5a78198047c09952a3274df9ba0586
nkc.kazuki.nakashima <nkc.kazuki.nakashima@gmail.com>
2019-07-18 14:19:31 +0900

    適当に試行錯誤中

---
+++ Car Game/Assets/4.nakashima/CarAction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAction : MonoBehaviour
+{
+    //吹っ飛ぶアクションさせるオブジェクト
+    public Rigidbody EnemyObj;
+
+    public float Actionspeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision colobj)
+    {
+        //Playerと接触したら
+        if (colobj.gameObject.tag == "Player")
+        {
+            //↑方向に飛ばす
+            EnemyObj.AddForce(transform.up * Actionspeed);
+        }
+    }
+}
