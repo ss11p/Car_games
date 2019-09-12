9d2f475de2f4ebfb37ce8bf04d07461cf0234e91
DESKTOP-28F38VP\user <nkc.syougo.sawada@cmail.com>
2019-09-12 12:29:40 +0900

    失敗した

---
+++ Car Game/Assets/3.SAWADA/Script/Pozu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pozu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pozuUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            pozuUI.SetActive(!pozuUI.activeSelf);
+            if (pozuUI.activeSelf)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+        }
+    }
+}
