b0c27505786e494e47251ab095c57c73601c5ed5
nkc.kazuki.nakashima <nkc.kazuki.nakashima@gmail.com>
2019-10-01 10:18:48 +0900

    オブジェクトのActiveをランダムでTrueにするのが可能になりました。

---
+++ Car Game/Assets/4.nakashima/ObjecetActive.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjecetActive : MonoBehaviour
+{
+    //ActiveをfalseにしてあるObjを入れる（個数は自由）
+    public GameObject[] ActiveObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          
+    }
+
+    public void Active()
+    {
+        //呼び出す際、これを呼び出すScriptに入れる
+        //public ObjecetActive objecetActive;
+        //objecetActive.Active();
+
+
+        //基本こちらで呼び出し
+
+        //0～オブジェクトの最大数まで
+        var ObjNumber = Random.Range(0, ActiveObj.Length);
+        //一度だけ呼び出し
+        ActiveObj[ObjNumber].SetActive(true);
+        Debug.Log(ActiveObj[ObjNumber] + "呼び出し成功");
+    }
+}
---
+++ Car Game/Assets/4.nakashima/test11.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test11 : MonoBehaviour
+{
+    public ObjecetActive objecetActive;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        //呼び出されているかの確認
+        objecetActive.Active();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
