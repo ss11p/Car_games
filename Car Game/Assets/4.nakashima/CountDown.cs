65e0858b748f8f463836642d9858878e862ae45e
nkc.kazuki.nakashima <nkc.kazuki.nakashima@gmail.com>
2019-10-03 10:21:28 +0900

    CountDownがカウントダウンさせるScript＋一部使わなさそうなScript消したので衝突したらそこはそちら側優先させてください

---
+++ Car Game/Assets/4.nakashima/CountDown.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    //カウント3秒
+    public float Count = 4;
+    //少数を出さないようにする
+    int second;
+    //表示するText
+    public Text TimerText;
+
+    //Findで取得したオブジェクトを入れる
+    public GameObject PlayerObj;
+    //カウントダウンを止めるBool
+    bool CountBool = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //PlayerのTag検索
+        PlayerObj = GameObject.Find("F1_root");
+        //var obj = PlayerObj.GetComponent<Car>();
+        if(CountBool == true)
+        {
+            Count -= Time.deltaTime;           
+        }
+        second = (int)Count;
+        //textに表示
+        TimerText.text = second.ToString();
+
+        if (second == 3)
+        {
+            //SEとかそういうの入れるならここ
+        }
+        if (second == 2)
+        {
+            
+        }
+        if (second == 1)
+        {
+
+        }
+
+        if (second == 0)
+        {
+            //カウントダウンを止める
+            CountBool = false;
+            
+            //プレイヤーを移動させるScriptをTrueにする
+            PlayerObj.GetComponent<Car>().enabled = true;
+            //1秒後textを消す
+            Invoke("TextActive", 1);
+        }
+    }
+    void TextActive()
+    {
+        //カウントダウンが終わったらこのScriptを止める
+        this.GetComponent<CountDown>().enabled = false;
+        //カウントダウンのText（この場合アタッチ先がText）を消す
+        gameObject.SetActive(false);      
+    }
+}
--- Car Game/Assets/4.nakashima/ObjecetActive.cs
+++ Car Game/Assets/4.nakashima/ObjecetActive.cs
@@ -9,7 +9,11 @@ public class ObjecetActive : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        //0～オブジェクトの最大数まで
+        var ObjNumber = Random.Range(0, ActiveObj.Length);
+        //一度だけ呼び出し
+        ActiveObj[ObjNumber].SetActive(true);
+        Debug.Log(ActiveObj[ObjNumber] + "呼び出し成功");
     }
 
     // Update is called once per frame
