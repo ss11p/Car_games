4985795d473c7624cf60101d86b9c46c77e59a9f
DESKTOP-28F38VP\user <nkc.syougo.sawada@cmail.com>
2019-10-03 10:20:38 +0900

    更新

---
+++ Car Game/Assets/3.SAWADA/Script/Sound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Sound : MonoBehaviour
+{
+    public bool DontDestroyEnabled = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (DontDestroyEnabled)
+        {
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Main")
+        {
+            DontDestroyEnabled = false;
+        }
+    }
+}
