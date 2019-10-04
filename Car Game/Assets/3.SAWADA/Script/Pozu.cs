d66875f9c0906d1bc84401235a5497e824581596
DESKTOP-28F38VP\user <nkc.syougo.sawada@cmail.com>
2019-10-04 13:46:54 +0900

    BGMを付けました

--- Car Game/Assets/3.SAWADA/Script/Pozu.cs
+++ Car Game/Assets/3.SAWADA/Script/Pozu.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Pozu : MonoBehaviour
 {
+    public AudioClip sound1;
+    public AudioClip sound2;
+    AudioSource audioSource;
+
     [SerializeField]
     public GameObject pozuCanvas;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -17,13 +21,16 @@ public class Pozu : MonoBehaviour
     {
         if (Input.GetKeyDown("q"))
         {
+            
             pozuCanvas.SetActive(!pozuCanvas.activeSelf);
             if (pozuCanvas.activeSelf)
             {
+                audioSource.PlayOneShot(sound1);
                 Time.timeScale = 0f;
             }
             else
             {
+                audioSource.PlayOneShot(sound2);
                 Time.timeScale = 1f;
             }
         }
