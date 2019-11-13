0ddf160f0cdc3d064d87806eb1884c2907085c78
DESKTOP-28F38VP\user <nkc.syougo.sawada@cmail.com>
2019-11-13 11:05:05 +0900

    新ステージ

---
+++ Car Game/Assets/3.SAWADA/Script/ChekPoint3.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChekPoint3 : MonoBehaviour
+{
+    public GameObject thisChekPoint;
+    public GameObject NextChekPoint;
+    bool Checkbool = true;
+    GameObject Chekpointes;
+    Chekpoints scpit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Chekpointes = GameObject.Find("ChekPoints");
+        scpit = Chekpointes.GetComponent<Chekpoints>();
+    }
+    void OnTriggerEnter(Collider coll)
+    {
+        if (Checkbool == true)
+        {
+            if (coll.gameObject.tag == "Car")
+            {
+
+                Nimotu3.nimotu3+= 1;
+                scpit.Active();
+                thisChekPoint.SetActive(false);
+                if (scpit.ActiveObj[0].activeSelf == false)
+                {
+                    if (scpit.ActiveObj[1].activeSelf == false)
+                    {
+                        if (scpit.ActiveObj[2].activeSelf == false)
+                        {
+                            if (scpit.ActiveObj[3].activeSelf == false)
+                            {
+                                if (scpit.ActiveObj[4].activeSelf == false)
+                                {
+                                    NextChekPoint.SetActive(true);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Car Game/Assets/3.SAWADA/Script/Nimotu3.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Nimotu3 : MonoBehaviour
+{
+    public Text NimotuText;
+    public static int nimotu3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nimotu3 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NimotuText.text = "=" + nimotu3;
+    }
+}
